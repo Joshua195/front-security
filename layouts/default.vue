1d47b7861496b4787b0f3c520a9dd37833e41f1d
Josue Gonzalez <gojoshua195@gmail.com>
2018-06-12 10:13:49 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container fluid fill-height>
+        <nuxt/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+
+    props: {
+      source: String
+    }
+  }
+</script>
